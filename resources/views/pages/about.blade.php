ee7ac585fc0dabbcb492bb145a6be8d4e62f9180
LQTam <laquyettam1995@gmail.com>
2019-11-24 12:06:04 +0700

    init project

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+@section('title','About')
+@section('content')
+    <div class="container">
+        <div class="content">
+            <div class="title">About Page</div>
+            <div class="title">Our about page!</div>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/pages/contact.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+@section('title','About')
+@section('content')
+    <div class="container">
+        <div class="content">
+            <div class="title">Contact Page</div>
+            <div class="title">Our contact page!</div>
+        </div>
+    </div>
+@endsection
